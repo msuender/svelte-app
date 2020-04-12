8ac66530348f0c1c1415e28ec2b6018cbfc64fd7
msuender <markus@suender.at>
2020-04-12 21:02:16 +0200

    initial commit

---
+++ src/Comp.svelte
@@ -0,0 +1,16 @@
+<script>
+    let count = 0;
+
+    const handleClick = () => {
+        count++;
+    }
+</script>
+
+<style>
+
+</style>
+
+<p>Mein erster Text</p>
+
+<button on:click={handleClick}>Clicked {count} times </button>
+
