fc8c214b0a861abb66e6b733e5acfcdab2ccc82c
msuender <markus@suender.at>
2020-04-13 17:57:14 +0200

    static list

--- src/Staticlist.svelte
+++ src/Staticlist.svelte
@@ -1,4 +1,6 @@
 <script>
+    import Mylist from './sub/Mylist.svelte';
+
     let list = [
         {"name": "Ans", "preis": 1},
         {"name": "Zwa", "preis": 2},
@@ -13,4 +15,8 @@
 {#each list as item}
      <p>Name: {item.name} Preis: {item.preis}</p>
 {/each}
+
+<h2>Static list with sub-component</h2>
+
+<Mylist mylist={list}/>
 
---
+++ src/sub/Mylist.svelte
@@ -0,0 +1,17 @@
+<script>
+
+    export let mylist = [
+        {"name": "Uno", "preis": 1},
+        {"name": "Due", "preis": 2},
+        {"name": "Tre", "preis": 3},
+    ]
+
+</script>
+
+{#each mylist as item}
+     <p>Name: {item.name} Preis: {item.preis}</p>
+{/each}
+
+
+
+
